c36bfab12ce6f218d45012948da5a486bf55d29a
chanki <bucheongosok@gmail.com>
2022-08-12 19:43:14 +0900

    완료

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
 import './app.css';
-import src from "./img/test1.jpg";
 import * as faceapi from 'face-api.js';
 
 const styles = { width: "300px" };
@@ -11,25 +10,45 @@ function App() {
 
   const [ init, setInit ] = useState(false);
   const [ imgSrc, setImgSrc ] = useState("");
-
+  const [ expression, setExpression ] = useState({ });
+  const keyNames = [ "angry", "disgusted", "fearful", "happy", "neutral", "sad", "surprised"  ];
+  
   useEffect(() => {
     const loadModels = async () => {
-      const MODEL_URL = process.env.PUBLIC_URL + "/models"; // 모델 경로
-
       Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
-        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
-        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
-        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
-      ]).then( () => detect() );
+        faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("./models")
+      ]).then( () => {
+        detect();
+        setInit(true);
+      });
     }
     imgSrc && loadModels();
   }, [imgSrc]);
 
   const detect = async () => {
-    const detections = await faceapi.detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-    console.log(detections[0]);
-    setInit(true);
+    const detection = await faceapi.detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+    const result = detection[0].expressions;
+    const resultArray = [];
+    let value;
+   
+    keyNames.map((key) => {
+      value = Math.round(result[`${key}`] * 100);
+      resultArray.push({ key, value });
+    });
+ 
+    resultArray.sort(function(a, b) { // 오름차순
+      return a.value > b.value ? -1 : a.value < b.value ? 1 : 0;
+    });
+
+    let obj = {}
+    let key = resultArray[0].key;
+    let val = resultArray[0].value;
+    obj[`${key}`] = val;
+
+    setExpression(obj);
   }
 
   const onChange = async evt => {
@@ -49,6 +68,7 @@ function App() {
     <div className="App">
       <h1>{ init ? "감지 끝" : "대기 중" }</h1>
       <h2>{ imgSrc ? "이미지 로드 완료" : "이미지 없음" }</h2>
+      <h3>{ Object.keys(expression).length !== 0 ? `state값 : ${Object.keys(expression)}, ${expression[Object.keys(expression)]}` : "state값 없음" }</h3>
       <img ref={imgRef} src={imgSrc} alt="" style={styles}/>
 
       <input ref={uploadRef} onChange={onChange} type="file" id="upload" accept="image/*" name="file" hidden={true}/>
